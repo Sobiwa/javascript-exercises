cfb08c5b1502b8df72f62f4e86cb9c8b00eac93f
Sobiwa <kevin.b.collister@gmail.com>
2022-09-09 10:01:19 -0400

    Convert temperature

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  const C = (F -32) * (5/9);
+  const Celsius = parseFloat(C.toFixed(1));
+  return Celsius;
 };
 
-const ctof = function() {
+const ctof = function(C) {
+  const F = C * (9/5) + 32;
+  const Fahrenheit = parseFloat(F.toFixed(1));
+  return Fahrenheit;
 
 };
 
